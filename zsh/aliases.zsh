b5a00425e4d9b18e2d690413ec96419db42661ca
Hamza Muraj <hamzamuraj@gmail.com>
2020-05-12 19:14:05 -0500

    default python and colored cat

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,4 @@
 alias ll='ls -lhG'
 alias locate='mdfind'
+alias python='/usr/local/bin/python3.8'
+alias pcat='/Library/Frameworks/Python.framework/Versions/3.8/bin/pygmentize -f terminal256 -O style=native -g'
